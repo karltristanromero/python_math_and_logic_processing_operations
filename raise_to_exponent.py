543ac7230c9f8783ee3b62e56dc5b9f5892872aa
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:22:09 +0800

    Add pseudocode for raise_to_exponent

--- raise_to_exponent.py
+++ raise_to_exponent.py
@@ -1,1 +1,8 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+'''
+    input 2 numbers
+    calculate the first_num raised to the second_num
+    print the result
+
+'''
