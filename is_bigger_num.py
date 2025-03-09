485e3f64508abd9f05b0f95ebe57cf0dad2626b6
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 22:54:25 +0800

    docs: create pseudocode for is_bigger_num

---
+++ is_bigger_num.py
@@ -0,0 +1,7 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+'''
+    input 2 numbers
+    if number is greater than the other
+        print result    
+'''
