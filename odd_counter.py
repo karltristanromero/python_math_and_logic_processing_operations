3def5274b932713d368793ec4552ff1f541066e5
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:38:39 +0800

    Add code that counts how many odds in numerical inputs

--- odd_counter.py
+++ odd_counter.py
@@ -7,3 +7,12 @@
         if odd (use modulo)
             add 1 to odd_counter
 '''
+
+odd_counter = 0
+
+for i in range(10):
+    num = float(input(f"({i+1}) Enter a number: "))
+    if num % 2 != 0:
+        odd_counter += 1
+
+print(f"There are {odd_counter} odd numbers.")
