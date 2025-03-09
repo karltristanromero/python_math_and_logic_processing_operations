ca042844a3109384fd34f892f6ba747e74252d26
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:10:24 +0800

    Add pseudocode for multiply_numbers

---
+++ multiply_numbers.py
@@ -0,0 +1,9 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+'''
+
+    input 2 numbers
+    multiply the numbers
+    print product
+    
+'''
