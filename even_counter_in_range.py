c40282a9229bfec198e3b98bc21aacb5e2b24052
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:43:28 +0800

    Add pseudocode for even_counter_in_range

--- even_counter_in_range.py
+++ even_counter_in_range.py
@@ -1,1 +1,9 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+'''
+    set even_counter
+    for loop in range 101 
+        if even (use modulo)
+            add 1 to even_counter
+    print even_counter
+'''
