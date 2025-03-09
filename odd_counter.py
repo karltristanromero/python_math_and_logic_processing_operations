b31fb928b2a6d40ee708044e4bbeba3fc3f19fe5
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:35:53 +0800

    Add pseudocode for odd_counter

--- odd_counter.py
+++ odd_counter.py
@@ -1,1 +1,9 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+'''
+    set odd_counter
+    for loop
+        input number
+        if odd (use modulo)
+            add 1 to odd_counter
+'''
