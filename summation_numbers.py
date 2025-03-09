da1a52aefcb3c7ca7cce0d75b00c82cab531e859
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:28:55 +0800

    Add pseudocode for summation_numbers

--- summation_numbers.py
+++ summation_numbers.py
@@ -1,1 +1,9 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+'''
+    set sum container
+    for loop with range of 10
+        input number
+        add number to sum
+
+'''
