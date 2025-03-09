84b81c37c947187c8eca189bacdfbf7eaedb4061
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:01:28 +0800

    create pseudocode for is_equal

--- is_equal.py
+++ is_equal.py
@@ -1,1 +1,9 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+'''
+
+    input 2 numbers
+    if numbers are equal
+    print "equal"
+
+'''
