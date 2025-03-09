b01d8a9ac504b9cf56179ccb1254076cbab23140
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 23:52:07 +0800

    Add pseudocode for filter_no_zero_end

--- filter_no_zero_end.py
+++ filter_no_zero_end.py
@@ -1,1 +1,8 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+'''
+    for loop in range 101
+        if number does not end in zero (use modulo 10)
+            print number
+
+'''
