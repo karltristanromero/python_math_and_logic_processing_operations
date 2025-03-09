47f9bd5840e8454ea5b1e61448262304f5c86941
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-09 22:58:35 +0800

    add the code to determine which inputter number is bigger.

--- is_bigger_num.py
+++ is_bigger_num.py
@@ -5,3 +5,11 @@
     if number is greater than the other
         print result    
 '''
+
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+if first_number > second_number:
+    print(f"The number {first_number} is bigger than {second_number}.")
+elif second_number > first_number:
+    print(f"The number {second_number} is bigger than {first_number}.")
