1c3561f5f80b4ccac240d91ec0d6fba23fd83fc1
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 20:50:40 +0800

    docs: Add pseudocode for is_unique_or_is_duplicate

--- is_unique_or_is_duplicate.py
+++ is_unique_or_is_duplicate.py
@@ -1,3 +1,19 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. 
 # Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when 
 # the inputted number have duplicate.--is_unique_or_is_duplicate
+
+'''
+    set list_num
+    while loop
+        input number
+        append number to list_num
+
+        try-except
+            if number in list_num
+                print duplicate
+            else
+                print unique
+        except Type Error
+            break loop
+
+'''
