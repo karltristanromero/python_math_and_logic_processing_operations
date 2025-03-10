a46893cc512c53407b96759250579fdea24194f9
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 16:46:42 +0800

    docs: Add pseudocode for numbers_no_duplicates

--- numbers_no_duplicates.py
+++ numbers_no_duplicates.py
@@ -1,1 +1,11 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+'''
+    set list of numbers
+    for loop in range(10)
+        input number
+        if number in list of numbers
+            pop the number from list of number
+    print(list of numbers)
+
+'''
