8bc9dfb317ef18f4dfd4d80266bf31af32f61028
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 11:31:34 +0800

    Add code that calculates for the difference of numerical inputs to the first input

--- first_minus_all.py
+++ first_minus_all.py
@@ -2,8 +2,20 @@
 '''
     set difference
     for loop in range 10
+        input number
         if first input
             add to difference
         else
             subtract to diff
 '''
+
+difference = 0
+
+for i in range(10):
+    num = float(input("Enter a number: "))
+    if i == 0:
+        difference += num
+    else:
+        difference -= num
+
+print(f"The difference of all numbers to the first input is: {difference}")
