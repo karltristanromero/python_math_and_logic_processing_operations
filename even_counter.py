0cb1f59be6fbed8a33c94156e8302ffd7baddc11
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 11:33:33 +0800

    docs: Add pseudocode to even_counter

--- even_counter.py
+++ even_counter.py
@@ -1,1 +1,8 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+'''
+    set even counter
+    for loop in range 10
+        if even
+            add 1 to even
+    print even counter
+'''
