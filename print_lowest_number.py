6694d23ebbb4f77a06724aaf7316c7a593d0594f
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 21:09:02 +0800

    docs: Add pseudocode to print_lowest_number

--- print_lowest_number.py
+++ print_lowest_number.py
@@ -1,1 +1,12 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number --print_lowest_number
+'''
+    set lowest_number to None
+    while loop
+        input num
+        try-except
+            if num is less than lowest_number
+                change lowest number's value with num's
+        excep Value error
+            break
+
+'''
