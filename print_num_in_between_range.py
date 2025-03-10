dfb94cc89cdf7d0916c4c09bd63797465370ad8f
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 12:00:39 +0800

    docs: Add pseudocode to print_num_in_between_range

--- print_num_in_between_range.py
+++ print_num_in_between_range.py
@@ -1,1 +1,6 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+'''
+    input for lower and upper boundaries of range
+    for num between range of lower and upper boudnaries:
+        print num
+'''
