f95b40430f85fc08cb621928039e17e2227ae6f3
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 11:03:16 +0800

    docs: Add pseudocode for subtract_numbers

--- subtract_numbers.py
+++ subtract_numbers.py
@@ -1,1 +1,7 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+'''
+    input 2 numbers
+    subtract the minuend to the subtrahend
+    print result
+'''
