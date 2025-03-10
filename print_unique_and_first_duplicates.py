cf31b47b912723816cc0a67eced8c11609fc0252
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 20:44:40 +0800

    Add code that will print all the numbers and exclude the duplicates

--- print_unique_and_first_duplicates.py
+++ print_unique_and_first_duplicates.py
@@ -11,3 +11,13 @@
 
 
 '''
+
+list_num_inputs = []
+
+for i in range(10):
+    num = input(f"({i+1}) Enter a number: ")
+    if num not in list_num_inputs:
+
+        list_num_inputs.append(num)
+
+print(f"\nRemoving the duplicates you get: {', '.join(list_num_inputs)}")
