a52ada657ffd978112ee088dde717047b2e2b8d0
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 10:57:32 +0800

    Add code that compares and prints out the numerical input

--- is_smaller_num.py
+++ is_smaller_num.py
@@ -7,3 +7,11 @@
         print number
 
 '''
+
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+if first_number < second_number:
+    print(f"The number {first_number} is smaller than {second_number}.")
+elif second_number < first_number:
+    print(f"The number {second_number} is smaller than {first_number}.")
