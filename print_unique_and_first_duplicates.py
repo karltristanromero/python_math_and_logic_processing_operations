2bf7122bc6ae19bb3a40fc7a0d4464adf3d04fee
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 20:24:39 +0800

    docs: Add pseudocode for print_unique_and_first_duplicates

---
+++ print_unique_and_first_duplicates.py
@@ -0,0 +1,13 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+'''
+    set a list of numerical inputs
+    for loop in range 10
+        input numbers
+        if number not in list
+            add number to list
+
+    print list
+
+
+'''
