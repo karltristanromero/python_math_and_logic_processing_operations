5cd6db9d3cbfd0c0c54088a9d98717c7977a7939
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 11:39:11 +0800

    docs: Add pseudocode to print_odd_in_range

--- print_odd_in_range.py
+++ print_odd_in_range.py
@@ -1,1 +1,9 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+'''
+    set range_counter
+    set odd_counter
+
+    while range_counter >= 100
+        if num is odd
+            add + 1 o odd_counter
+'''
