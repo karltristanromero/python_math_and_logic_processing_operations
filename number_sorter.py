ec10a89b88e003c5bc1fd1459bc02c97220d16c4
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 21:19:36 +0800

    docs: Add pseudocode for number_sorter

--- number_sorter.py
+++ number_sorter.py
@@ -1,1 +1,13 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function --number_sorter
+'''
+    set list_num
+
+    while loop
+        try-except
+            input number
+        except valueerror
+            break
+    
+    sort list_num and store in sortetd_list
+    print sorted_list
+'''
