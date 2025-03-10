d76d71fe8409295045bf59b02a551417481b31a0
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 11:11:41 +0800

    Create program file that will print the remainder of numerical inputs

--- print_remainder.py
+++ print_remainder.py
@@ -1,1 +1,6 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+'''
+    input dividend and divisor
+    calculate remainder, use modulo
+    print remainder
+'''
