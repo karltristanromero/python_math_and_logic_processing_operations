4f1abd2ec247121e2f7e1db65b7f970f42be0206
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 11:09:31 +0800

    Add code that prints quotient without decimals

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -1,1 +1,12 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+'''
+    input 2 numbers
+    divide the dividend to the divisor
+    print result without decimal
+'''
+
+dividend = float(input("Enter the dividend: "))
+divisor = float(input("Enter the divisor: "))
+    
+quotient = dividend / divisor
+print(f"{dividend} divided by {divisor} is {int(quotient)}")
