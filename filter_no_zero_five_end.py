74c16238850873ab624d93f344a5acd2e6ee6bfa
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 11:54:10 +0800

    docs: Add pseudocode for filter_no_zero_five

--- filter_no_zero_five_end.py
+++ filter_no_zero_five_end.py
@@ -1,1 +1,9 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+'''
+    set num to 0
+    while loop in range 0-100
+        if num doesn't end in zero/five
+            print num
+        add + 1 to num
+'''
