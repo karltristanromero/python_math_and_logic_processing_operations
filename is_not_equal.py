5584d810e11b3e416181e5554064537419f4675e
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-10 10:59:45 +0800

    docs: Add pseudocode for is_not_equal

--- is_not_equal.py
+++ is_not_equal.py
@@ -1,1 +1,9 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+'''
+
+    input 2 numbers
+    if the numbers are not equal
+        print "not equal"
+
+'''
