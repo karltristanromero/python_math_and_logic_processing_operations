a67e91632ccdaaf2e6f9a9f1ff6e324a56b16301
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-11 16:11:29 +0800

    docs: Rename the directory for the batch 3 of python prorgamming exercises from '...numeric...' into '...value...' to improve the repository's structure clarity

---
+++ simple_value_manipulations_to_filter_and_sort/is_unique_or_is_duplicate.py
@@ -0,0 +1,37 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when 
+# the inputted number have duplicate.--is_unique_or_is_duplicate
+
+'''
+    set list_num
+    while loop
+        try-except
+
+            input number
+
+            if number in list_num
+                print duplicate
+            else
+                append number to list_num
+                print unique
+
+        except ValueError
+            break loop
+
+
+'''
+
+list_numbers = []
+
+while True:
+    try:
+        number = float(input("\nEnter a number: "))
+
+        if number in list_numbers:
+            print(f"{number} is a DUPLICATE!")
+        else:
+            list_numbers.append(number)
+            print(f"{number} is UNIQUE!")
+    except ValueError:
+        print("Program shuttind down...\n")
+        break
