f58061ceb8cc938f398411d110db384624072ea6
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 01:01:11 +0800

    docs: Update pseudocode to fix the logical error in the output wherein excess duplicates are printed instead of just one duplicate

--- simple_value_handling_to_compare_and_sort/print_duplicates_only.py
+++ simple_value_handling_to_compare_and_sort/print_duplicates_only.py
@@ -1,26 +1,29 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 '''
     set list that will store all the numerical inputs
-    set list for duplicate numbers
     for loop in range 10
         input number
-        if number is not found in list of numerical inputs
-            append number to list of numerical inputs
-        elif number is found in list of numerical inputs
-            append to duplicate numbers
+        append number to list of all numerical inputs
+    
+    set list of duplicate numbers
+    set list of num without duplicates
+
+    for loop in range of list of numerical inputs
+        if num not in num without duplicates
+            append num to list of num without duplicates
+        elif num in num without duplicates and num not in duplicate numbers
+            append num to list of duplicate numbers
+
     
     print(duplicate numbers)
 
 '''
 
 list_all_num = []
-duplicate_num = []
 
 for i in range(10):
     number = float(input(f"({i+1}) Enter a number: "))
-    if number not in list_all_num:
-        list_all_num.append(number)
-    else:
-        duplicate_num.append(number)
+    list_all_num.append(number)
+
 
 print(duplicate_num)
