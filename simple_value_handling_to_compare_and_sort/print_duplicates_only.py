c2781662113619548bd0bd264527cb3df4613f10
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 00:48:38 +0800

    docs: Add pseudocode for print_duplicates_only

--- simple_value_handling_to_compare_and_sort/print_duplicates_only.py
+++ simple_value_handling_to_compare_and_sort/print_duplicates_only.py
@@ -1,1 +1,14 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+'''
+    set list that will store all the numerical inputs
+    set list for duplicate numbers
+    for loop in range 10
+        input number
+        if number is not found in list of numerical inputs
+            append number to list of numerical inputs
+        elif number is found in list of numerical inputs
+            append to duplicate numbers
+    
+    print(duplicate numbers)
+
+'''
