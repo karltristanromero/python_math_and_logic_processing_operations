55a6798459defef327ea0db462133790020b6763
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 11:26:29 +0800

    docs: Add pseudocode for descending_number_sorter

--- simple_value_handling_to_compare_and_sort/descending_number_sorter.py
+++ simple_value_handling_to_compare_and_sort/descending_number_sorter.py
@@ -1,1 +1,14 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+'''
+    set list for all numerical inputs
+
+    while loop
+        try-except
+            input number
+            append number to list of numerical inputs
+        excepy Value Error
+            break the while loop
+    
+    sort the list
+    print list
+'''
