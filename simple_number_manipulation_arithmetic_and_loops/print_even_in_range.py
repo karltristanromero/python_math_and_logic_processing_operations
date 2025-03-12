0e76f7cee3d607bb29961cb19f7cb09c14df4d7b
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 19:11:09 +0800

    style: Left-align the pseudocode and added a newline for better readability

--- simple_number_manipulation_arithmetic_and_loops/print_even_in_range.py
+++ simple_number_manipulation_arithmetic_and_loops/print_even_in_range.py
@@ -1,11 +1,12 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
-'''
-    for loop in range 101 
-        if number is even (use modulo)
-            print num
-'''
+# Pseudocode
+# - for loop in range 101 
+# - if number is even (use modulo)
+# - print num
 
 for num in range(101):
     if num % 2 == 0:
         print(num, end=" ")
+
+print()
