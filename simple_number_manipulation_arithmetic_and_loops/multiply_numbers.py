982d87eb17debfde51863495434eae6aeb04c58c
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 17:50:03 +0800

    style: Left-aligned the pseudocode for multiply_numbers and changed the variable names for operation clarity

--- simple_number_manipulation_arithmetic_and_loops/multiply_numbers.py
+++ simple_number_manipulation_arithmetic_and_loops/multiply_numbers.py
@@ -1,13 +1,14 @@
-# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+# Prog04: Create a program that ask user to input 2 numbers. Print the product 
+# of the two numbers.
 
-'''
-
-    input 2 numbers
-    multiply the numbers
-    print product
+# Pseudocode
+# - input 2 numbers
+# - multiply the two numbers
+# - print product
     
-'''
-first_number = float(input("Enter the multiplicand: "))
-second_number = float(input("Enter the multiplier: "))
+multiplicand = float(input("Enter the multiplicand: "))
+multiplier = float(input("Enter the multiplier: "))
 
-print(f"Product: {first_number * second_number}")
+product = multiplicand * multiplier
+
+print(f"The product of {multiplicand} and {multiplier} is {product}")
