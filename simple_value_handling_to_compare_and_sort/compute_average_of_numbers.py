ffd42c9639396adf58d2db9f378ec62271fdfd16
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:13:57 +0800

    style: Left-aligned pseudocode for better readability

--- simple_value_handling_to_compare_and_sort/compute_average_of_numbers.py
+++ simple_value_handling_to_compare_and_sort/compute_average_of_numbers.py
@@ -1,20 +1,19 @@
-# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average. 
-'''
-    set total_sum of numerical inputs to 0
-    set frequency_counter to track the number of items
+# Prog05: Create a program that ask user to input a number, continue asking 
+# until the user input is invalid. Display the average. 
 
-    while loop
-        try-except
-            input number
-            add number to total sum
-            add 1 to frequency counter
-        except ValueError
-    
-# Compute for average:
-    set average equal to total_sum divided by frequency_counter
+# Pseudocode
+# - set total_sum of numerical inputs to 0
+# - set frequency_counter to track the number of items
 
-    print average
-'''
+# - while loop
+# - try-except
+# - input number
+# - add number to total sum
+# - add 1 to frequency counter
+# - except ValueError
+
+# - set average equal to total_sum divided by frequency_counter
+# print average
 
 total_sum = 0
 frequency_counter = 0
