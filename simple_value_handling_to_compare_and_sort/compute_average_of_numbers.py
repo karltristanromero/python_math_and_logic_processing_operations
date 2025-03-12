2c175f84b8ced171bc04a1ace1e63c1c55e9a004
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 11:35:18 +0800

    docs: Add pseudocode for computer_average_of_numbers

--- simple_value_handling_to_compare_and_sort/compute_average_of_numbers.py
+++ simple_value_handling_to_compare_and_sort/compute_average_of_numbers.py
@@ -1,1 +1,16 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average. 
+'''
+    set total_sum of numerical inputs to 0
+    set frequency_counter to track the number of items
+
+    while loop
+        try-except
+            input number
+            add number to total sum
+        except ValueError
+    
+# Compute for average:
+    set average equal to total_sum divided by frequency_counter
+
+    print average
+'''
