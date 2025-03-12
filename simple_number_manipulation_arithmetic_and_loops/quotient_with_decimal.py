e6fb3687997bdbe28186bd500769a96a72097400
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 19:56:57 +0800

    style: Left-aligned the pseudocode to improve its clarity

--- simple_number_manipulation_arithmetic_and_loops/quotient_with_decimal.py
+++ simple_number_manipulation_arithmetic_and_loops/quotient_with_decimal.py
@@ -1,16 +1,17 @@
-# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+# Prog05: Create a program that ask user to input 2 numbers. Print the 
+# quotient of the two numbers with the decimal point
 
-'''
-    input 2 numbers
-    divide the first to the second number
-    print the quotient with decimal
+# Pseudocode
+# - input 2 numbers
+# - divide the first to the second number
+# - print the quotient with decimal
 
-'''
 dividend = float(input("Enter the dividend: "))
 divisor = float(input("Enter the divisor: "))
 
 try:
     quotient = dividend / divisor
     print(f"The quotient of {dividend} ÷ {divisor} is {quotient}")
+
 except ZeroDivisionError:
     print("You cannot divide by zero!")
