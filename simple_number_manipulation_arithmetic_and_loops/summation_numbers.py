85905fa24045bb7d6a96c7cf95b0551a71eda101
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:03:08 +0800

    refactor: Changed sum into total_sum to avoid clashing with python's sum()

--- simple_number_manipulation_arithmetic_and_loops/summation_numbers.py
+++ simple_number_manipulation_arithmetic_and_loops/summation_numbers.py
@@ -1,17 +1,17 @@
-# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of 
+# all the numbers.
 
-'''
-    set sum container
-    for loop with range of 10
-        input number
-        add number to sum
+# Pseudocode
+# - set sum container
+# - for loop with range of 10
+# - input number
+# - add number to sum
 
-'''
 
-sum = 0
+total_sum = 0
 
 for i in range(10):
     addend = float(input(f"({i + 1}) Enter an addend: "))
-    sum += addend
+    total_sum += addend
 
-print(f"Summing it all up, you get: {sum}!!")
+print(f"Summing it all up, you get: {total_sum}!!")
