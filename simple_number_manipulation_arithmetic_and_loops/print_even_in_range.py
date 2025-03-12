881698d0caabf6f129b78ece8708d161f33df464
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 19:14:20 +0800

    style: Split lines in the problem instruction for print_even_in_range in accordance with the coding standard

--- simple_number_manipulation_arithmetic_and_loops/print_even_in_range.py
+++ simple_number_manipulation_arithmetic_and_loops/print_even_in_range.py
@@ -1,4 +1,5 @@
-# Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+# Prog09: Create a program that print all the even numbers starting from 0 to 
+# 100. (Use for loop)
 
 # Pseudocode
 # - for loop in range 101 
