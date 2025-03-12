0f1c29394928249e7683edb80ba3810515520f1e
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:46:51 +0800

    style: Left-aligned pseudocode for code clarity and reduced the instruction line's characters to 79 per line

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/first_minus_all.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/first_minus_all.py
@@ -1,13 +1,15 @@
-# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
-'''
-    set difference
-    for loop in range 10
-        input number
-        if first input
-            add to difference
-        else
-            subtract to diff
-'''
+# Prog06: Create a program that ask user to input 10 numbers. Print the result 
+# of the first number minus all of the remaining numbers.
+
+# Pseudocode
+# - set difference
+# - for loop in range 10
+# - input number
+# - if first input
+# - add to difference
+# - else
+# - subtract to diff
+
 
 difference = 0
 
