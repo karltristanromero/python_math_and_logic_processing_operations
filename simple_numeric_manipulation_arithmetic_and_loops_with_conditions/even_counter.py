8bcdeadfbeb0c4156d968d18e375040aa34f2de1
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:44:15 +0800

    Splitted the instruction line to comply with the coding standard of 79 char per line for the two program files

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/even_counter.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/even_counter.py
@@ -1,4 +1,5 @@
-# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+# Prog07: Create a program that ask user to input 10 numbers. Print how many 
+# are even numbers.
 
 # Pseudocode
 # - set even counter
--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/filter_no_zero_five_end.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/filter_no_zero_five_end.py
@@ -1,4 +1,5 @@
-# Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+# Prog09: Create a program that print all the numbers starting from 0 to 100 
+# except numbers ending in zero or ending five.
 
 # - set num to 0
 # - while loop in range 0-100
