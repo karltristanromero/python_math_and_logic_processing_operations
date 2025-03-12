d22a3010952998581780947762119332948d4bf5
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:04:59 +0800

    style: Left-aligned pseudocode for code readability

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_remainder.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_remainder.py
@@ -1,9 +1,10 @@
-# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
-'''
-    input dividend and divisor
-    calculate remainder, use modulo
-    print remainder
-'''
+# Prog05: Create a program that ask user to input 2 numbers. Print the 
+# remainder when the first number is divided by the second number.
+
+# Pseudocode
+# - input dividend and divisor
+# - calculate remainder, use modulo
+# - print remainder
 
 dividend = float(input("Enter the dividend: "))
 divisor = float(input("Enter the divisor: "))
@@ -11,5 +12,6 @@ divisor = float(input("Enter the divisor: "))
 try:
     remainder = dividend % divisor
     print(f"The remainder of {dividend} ÷ {divisor} is {remainder}")
+
 except ZeroDivisionError:
     print("You cannot divide by zero!")
