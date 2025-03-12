469fa03c814970b0aced751b1786c9850ba6b3e3
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:42:37 +0800

    refactor: Change the while loop condition to improve readability using a comparison operator instead of a statement

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/filter_no_zero_five_end.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/filter_no_zero_five_end.py
@@ -1,16 +1,14 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
-'''
-    set num to 0
-    while loop in range 0-100
-        if num doesn't end in zero/five
-            print num
-        add + 1 to num
-'''
+# - set num to 0
+# - while loop in range 0-100
+# - if num doesn't end in zero/five
+# - print num
+# - add + 1 to num
 
 num = 0
 
-while num in range(0, 100):
+while num < 101:
     if num % 5 != 0:
         print(num, end=" ")
     num += 1
