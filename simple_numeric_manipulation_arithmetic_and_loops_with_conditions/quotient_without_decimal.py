568f806f63c4ced9324de68331d8232994317a11
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:07:10 +0800

    style: Left-aligned pseudocode  to improve code readability

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/quotient_without_decimal.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/quotient_without_decimal.py
@@ -1,9 +1,9 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
-'''
-    input 2 numbers
-    divide the dividend to the divisor
-    print result without decimal
-'''
+
+# Pseudocode
+# - input 2 numbers
+# - divide the dividend to the divisor
+# - print result without decimal
 
 dividend = float(input("Enter the dividend: "))
 divisor = float(input("Enter the divisor: "))
@@ -11,5 +11,6 @@ divisor = float(input("Enter the divisor: "))
 try:
     quotient = dividend / divisor
     print(f"{dividend} divided by {divisor} is {quotient:.0f}")
+    
 except ZeroDivisionError:
-    print("You cannot divided by zero!")
+    print("You cannot divide by zero!")
