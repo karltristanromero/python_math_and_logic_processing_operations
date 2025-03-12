1b36f5fe2a69ff9fc00bf4f0fc8234686fc6739a
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:09:37 +0800

    refactor: Change the output message to a shorter one for better program presentation

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/subtract_numbers.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/subtract_numbers.py
@@ -1,14 +1,13 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
-'''
-    input 2 numbers
-    subtract the minuend to the subtrahend
-    print result
-'''
+# Pseudocode
+# - input 2 numbers
+# - subtract the minuend to the subtrahend
+# - print result
 
 minuend = float(input("Enter the minuend: "))
 subtrahend = float(input("Enter the subtrahend: "))
 
 difference = minuend - subtrahend
 
-print(f"The difference of {minuend} and {subtrahend} is {difference}.")
+print(f"{minuend} minus {subtrahend} is {difference}.")
