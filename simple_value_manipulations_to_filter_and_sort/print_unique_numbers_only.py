d611aba29745567b2849fa9124324b8dfa9903dd
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 22:16:31 +0800

    style: Left-aligned pseudocode for code readability

--- simple_value_manipulations_to_filter_and_sort/print_unique_numbers_only.py
+++ simple_value_manipulations_to_filter_and_sort/print_unique_numbers_only.py
@@ -1,14 +1,14 @@
-# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+# Prog01: Create a program that ask user to input 10 numbers. Display all 
+# numbers that don't have duplicate.
 
-'''
-    set list of numbers
-    for loop in range(10)
-        input number
-        if number in list of numbers
-            pop the number from list of number
-    print(list of numbers)
+# Pseudocode
+# - set list of numbers
+# - for loop in range(10)
+# - input number
+# - if number in list of numbers
+# - pop the number from list of number
+# - print(list of numbers)
 
-'''
 list_of_num = []
 
 for i in range(10):
