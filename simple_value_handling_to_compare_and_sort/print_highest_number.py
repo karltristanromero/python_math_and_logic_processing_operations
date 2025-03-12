7ac828d3261c389bef460f30ac304f1e8999dc0b
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:38:38 +0800

    refactor: Combined the previous if-else into a single if statement to improve conciseness of program

--- simple_value_handling_to_compare_and_sort/print_highest_number.py
+++ simple_value_handling_to_compare_and_sort/print_highest_number.py
@@ -1,33 +1,34 @@
-# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+# Prog03: Create a program that ask user to input a number, continue asking 
+# until the user input is invalid. Display the highest number
 
-'''
-    set highest_number to None
+# - set highest_number to None
 
-    while loop
-        try-except
-            input number
-                if the highest_number is still set to None
-                    replace highest number with the input number
-                elif input number is greater than highest number
-                    replaca highest number with the input number
-        except a ValueError occurred
-            break the while loop 
+# - while loop
+# - try-except
+# - input number
+# - if the highest_number is still set to None
+# - replace highest number with the input number
+# - elif input number is greater than highest number
+# - replaca highest number with the input number
+# - except a ValueError occurred
+# - break the while loop 
 
-    print highest_number
-'''
+# - print highest_number
 
+# Main Program
 highest_number = None
 
 while True:
     try:
         number = float(input("Enter a number: "))
 
-        if highest_number == None:
-            highest_number = number
-        elif number > highest_number:
+        if highest_number is None or number > highest_number:
             highest_number = number
 
     except ValueError:
         break
 
-print(f"The highest number you have entered is: {highest_number}")
+if highest_number is not None:
+    print(f"The highest number you have entered is: {highest_number}")
+else:
+    print("There was no valid input entered.")
