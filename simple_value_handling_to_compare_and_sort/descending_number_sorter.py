6bb4e2741b79ccba3d03bcc20d7db5bc12e0e21e
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:24:12 +0800

    feat: Added a feature wherein an invalid input as the first input will be caught by the added if-else statement

--- simple_value_handling_to_compare_and_sort/descending_number_sorter.py
+++ simple_value_handling_to_compare_and_sort/descending_number_sorter.py
@@ -1,17 +1,19 @@
-# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
-'''
-    set list for all numerical inputs
+# Prog04: Create a program that ask user to input a number, continue asking 
+# until the user input is invalid. Display the number from highest to lowest. 
+# Clue: sort() function
 
-    while loop
-        try-except
-            input number
-            append number to list of numerical inputs
-        excepy Value Error
-            break the while loop
-    
-    sort the list
-    print list
-'''
+# Pseudocode
+# - set list for all numerical inputs
+
+# - while loop
+# - try-except
+# - input number
+# - append number to list of numerical inputs
+# - excepy Value Error
+# - break the while loop
+
+# - sort the list
+# - print list
 
 list_of_all_nums = []
 
@@ -22,6 +24,8 @@ while True:
     except ValueError:
         break
 
-list_of_all_nums.sort(reverse=True)
-
-print(f"I have sorted your numbers in descending order: {list_of_all_nums}")
+if list_of_all_nums:
+    list_of_all_nums.sort(reverse=True)
+    print(f"I sorted your numbers in descending order: {list_of_all_nums}")
+else:
+    print("No valid input was entered.")
