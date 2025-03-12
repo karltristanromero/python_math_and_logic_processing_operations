6ca715437a7ce469ce41d7ff3da32a2bc6f906c8
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:56:16 +0800

    feat: Added an error handling feature that catches if there were no valid inputs entered

--- simple_value_manipulations_to_filter_and_sort/number_sorter.py
+++ simple_value_manipulations_to_filter_and_sort/number_sorter.py
@@ -1,16 +1,18 @@
-# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function --number_sorter
-'''
-    set list_num
+# Prog05: Create a program that ask user to input a number, continue asking 
+# until the user input is invalid. Display the number from lowest to highest. 
+# Clue: sort() function --number_sorter
 
-    while loop
-        try-except
-            input number
-        except valueerror
-            break
-    
-    sort list_num
-    print list_num
-'''
+# Pseudocode
+# - set list_num
+
+# - while loop
+# - try-except
+# - input number
+# - except valueerror
+# - break
+
+# - sort list_num
+# - print list_num
 
 list_num = []
 
@@ -18,10 +20,14 @@ while True:
     try:
         number = float(input("Enter a number: "))
         list_num.append(number)
+
     except ValueError:
-        print("Printing results...")
+        if list_num:
+            print("Printing results...")
+        else:
+            print("No valid inputs were entered.")
         break
 
-list_num.sort()
-
-print(f"\nSorted numbers: {list_num}")
+if list_num:
+    list_num.sort()
+    print(f"\nSorted numbers: {list_num}")
