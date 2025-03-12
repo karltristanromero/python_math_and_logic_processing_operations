279f12249d974aaee398d0892c7a4a513835745a
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 17:41:38 +0800

    style: Left-align the pseudocode, add a header for it, and add blank line between the if-else statements to improve code readability"

--- simple_number_manipulation_arithmetic_and_loops/is_bigger_num.py
+++ simple_number_manipulation_arithmetic_and_loops/is_bigger_num.py
@@ -1,15 +1,16 @@
-# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger 
+# number.
 
-'''
-    input 2 numbers
-    if number is greater than the other
-        print result    
-'''
+# Pseudocode
+# - input 2 numbers
+# - if number is greater than the other
+# - print result    
 
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 if first_number > second_number:
     print(f"The number {first_number} is bigger than {second_number}.")
+    
 elif second_number > first_number:
     print(f"The number {second_number} is bigger than {first_number}.")
