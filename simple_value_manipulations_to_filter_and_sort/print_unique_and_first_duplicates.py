c3725801b125093836d10164de8a2faba2121c2d
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 22:15:43 +0800

    fix: Fix the error where str can be inputted, added a float function to change the input prompt from str to float

--- simple_value_manipulations_to_filter_and_sort/print_unique_and_first_duplicates.py
+++ simple_value_manipulations_to_filter_and_sort/print_unique_and_first_duplicates.py
@@ -1,23 +1,21 @@
-# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+# Prog02: Create a program that ask user to input 10 numbers. Display all 
+# numbers. For numbers with duplicate, display only the first entry.
 
-'''
-    set a list of numerical inputs
-    for loop in range 10
-        input numbers
-        if number not in list
-            add number to list
+# Pseudocode
+# - set a list of numerical inputs
+# - for loop in range 10
+# - input numbers
+# - if number not in list
+# - add number to list
 
-    print list
-
-
-'''
+# - print list
 
 list_num_inputs = []
 
 for i in range(10):
-    num = input(f"({i+1}) Enter a number: ")
-    if num not in list_num_inputs:
+    num = float(input(f"({i+1}) Enter a number: "))
 
+    if num not in list_num_inputs:
         list_num_inputs.append(num)
 
-print(f"\nRemoving the duplicates you get: {', '.join(list_num_inputs)}")
+print(f"\nRemoving the duplicates you get: {list_num_inputs}")
