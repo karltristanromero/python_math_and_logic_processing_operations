e38ce6b783cf0ff4c30218955e08b6bba9a7e2fb
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 17:31:40 +0800

    style: Adhere to coding standards by limiting to 79 characters per line

--- simple_number_manipulation_arithmetic_and_loops/add_numbers.py
+++ simple_number_manipulation_arithmetic_and_loops/add_numbers.py
@@ -1,13 +1,13 @@
-# Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+# Prog03: Create a program that ask user to input 2 numbers. Print the sum of 
+# the two numbers.
 
-'''
-    input numbers
-    add numbers
-    print result
-
-'''
+# Pseudocode
+# - input numbers
+# - add numbers
+# - print result
 
 first_number = float(input("Enter the first addend: "))
 second_number = float(input("Enter the second addend: "))
 
-print(f"The sum of the two numbers is: {first_number + second_number}")
+result = first_number + second_number
+print(f"The sum of the two numbers is: {result}")
