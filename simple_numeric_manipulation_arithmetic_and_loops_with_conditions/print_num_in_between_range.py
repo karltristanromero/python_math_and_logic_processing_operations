4431bdec7b211ca8fe31acaee2c01489b21a7a3a
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:55:43 +0800

    docs: Added the pseudocode headline to improve program clarity

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_num_in_between_range.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_num_in_between_range.py
@@ -1,6 +1,7 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the 
 # numbers between the two numbers.
 
+# Pseudocode
 # - input for lower and upper boundaries of range
 # - for num between range of lower and upper boudnaries:
 # - print num
