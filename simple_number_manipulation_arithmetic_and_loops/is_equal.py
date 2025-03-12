046e9c1e2a464ce66bcf291c23934aaf51becdef
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 17:45:22 +0800

    style: Left-aligned pseudocode and shortened print output statement for clarity and conciseness

--- simple_number_manipulation_arithmetic_and_loops/is_equal.py
+++ simple_number_manipulation_arithmetic_and_loops/is_equal.py
@@ -1,15 +1,14 @@
-# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" 
+# when the numbers are the same.
 
-'''
+# Pseudocode
+# - input 2 numbers
+# - if numbers are equal
+# - print "equal"
 
-    input 2 numbers
-    if numbers are equal
-    print "equal"
-
-'''
 
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 if first_number == second_number:
-    print(f"The numbers {first_number} and {second_number} are equal")
+    print(f"{first_number} and {second_number} are equal")
