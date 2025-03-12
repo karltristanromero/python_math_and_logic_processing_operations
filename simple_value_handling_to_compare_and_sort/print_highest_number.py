6ee9e071ea794300060f6d191006e3b2e245b77b
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 11:16:47 +0800

    docs: Add pseudocode for pring_highest_number

--- simple_value_handling_to_compare_and_sort/print_highest_number.py
+++ simple_value_handling_to_compare_and_sort/print_highest_number.py
@@ -1,1 +1,17 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+'''
+    set highest_number to None
+
+    while loop
+        try-except
+            input number
+                if the highest_number is still set to None
+                    replace highest number with the input number
+                elif input number is greater than highest number
+                    replaca highest number with the input number
+        except a ValueError occurred
+            break the while loop 
+            
+    print highest_number
+'''
