652707d3aa8a33c137d1cea33254f6e6864f2d7d
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:54:04 +0800

    refactor: Edited input prompt message for better program clarity

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_num_in_between_range.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_num_in_between_range.py
@@ -1,12 +1,12 @@
-# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
-'''
-    input for lower and upper boundaries of range
-    for num between range of lower and upper boudnaries:
-        print num
-'''
+# Prog10: Create a program that ask user to input 2 numbers. Print all the 
+# numbers between the two numbers.
 
-lower_boundary = input("Enter a boundary: ")
-upper_boundary = input("Enter a boundary: ")
+# - input for lower and upper boundaries of range
+# - for num between range of lower and upper boudnaries:
+# - print num
+
+lower_boundary = input("Enter lower boundary: ")
+upper_boundary = input("Enter upper boundary: ")
 
 lower_boundary = int(float(lower_boundary))
 upper_boundary = int(float(upper_boundary))
