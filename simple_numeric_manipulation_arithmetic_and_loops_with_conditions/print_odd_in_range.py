bca4101400f23a50e1bf09f07e9e23f63050672f
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:59:54 +0800

    refactor: Change the statement condition of while loop into a comparison operator for better readability

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_odd_in_range.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_odd_in_range.py
@@ -1,16 +1,15 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-'''
-    set num
 
-    while num <= 100
-        if num is odd
-            print(num)
-        add + 1 to range_counter
-'''
+# Pseudocode
+# - set num
+# - while num <= 100
+# - if num is odd
+# - print(num)
+# - add + 1 to range_counter
 
 num = 0
 
-while num in range(0, 101):
+while num <= 100:
     if num % 2 != 0:
         print(num, end=" " )
     num += 1
