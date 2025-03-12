7bd33641347d851f5474bc9c200e98890b63efc0
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 22:08:10 +0800

    style: Left-aligned pseudocode to improve code readability

--- simple_value_manipulations_to_filter_and_sort/print_lowest_number.py
+++ simple_value_manipulations_to_filter_and_sort/print_lowest_number.py
@@ -1,20 +1,22 @@
-# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number --print_lowest_number
-'''
-    set lowest_number to None
-    while loop
-        try except
-            input num
-            if lowest_number is still None
-                set lowest_number into num's value
-            elif num is less than lowest_number
-                change lowest number's value with num's
-        except Value error
-            break
-            
-    print(lowest_number)
+# Prog04: Create a program that ask user to input a number, continue asking 
+# until the user input is invalid. Display the lowest number 
+# --print_lowest_number
 
-'''
+# Pseudocode
+# - set lowest_number to None
+# - while loop
+# - try except
+# - input num
+# - if lowest_number is still None
+# - set lowest_number into num's value
+# - elif num is less than lowest_number
+# - change lowest number's value with num's
+# - except Value error
+# - break
 
+# - print(lowest_number)
+
+# Main Program
 lowest_number = None
 
 while True:
