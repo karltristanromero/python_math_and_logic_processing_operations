4eec966d41bb4846974b85777d7523d2f910d962
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 20:00:27 +0800

    style: Left-aligned pseudocode for code clarity and minimized number of characters per line to 79

--- simple_number_manipulation_arithmetic_and_loops/raise_to_exponent.py
+++ simple_number_manipulation_arithmetic_and_loops/raise_to_exponent.py
@@ -1,11 +1,10 @@
-# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+# Prog06: Create a program that ask user to input 2 numbers. Print the result 
+# when the first number is raised to the second number.
 
-'''
-    input 2 numbers
-    calculate the first_num raised to the second_num
-    print the result
-
-'''
+# Pseudocode
+# - input 2 numbers
+# - calculate the first_num raised to the second_num
+# - print the result
 
 first_number = float(input("Enter the base to be raised: "))
 second_number = float(input("Enter the exponent: "))
