c488fb0b9a430e27b7290432f08accdda0375f28
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:00:54 +0800

    style: Limited the instruction line down to 79 characters per line to comply with coding standards

--- simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_odd_in_range.py
+++ simple_numeric_manipulation_arithmetic_and_loops_with_conditions/print_odd_in_range.py
@@ -1,4 +1,5 @@
-# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+# Prog08: Create a program that print all the odd numbers starting from 0 to 
+# 100. (Use while loop)
 
 # Pseudocode
 # - set num
