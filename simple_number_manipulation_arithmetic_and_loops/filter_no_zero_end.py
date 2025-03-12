e9c08b251086f29de1aff868e265917f63743c4a
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 17:39:10 +0800

    style: Update pseudocode by left aligning it for readability and added newline (\n) at the end of program

--- simple_number_manipulation_arithmetic_and_loops/filter_no_zero_end.py
+++ simple_number_manipulation_arithmetic_and_loops/filter_no_zero_end.py
@@ -1,12 +1,13 @@
-# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+# Prog10: Create a program that print all the numbers starting from 0 to 100 
+# except numbers ending in zero.
 
-'''
-    for loop in range 101
-        if number does not end in zero (use modulo 10)
-            print number
-
-'''
+# Pseudocode
+# - for loop in range 101
+# - if number does not end in zero (use modulo 10)
+# - print number
 
 for num in range(101):
     if num % 10 != 0:
         print(num, end=" ")
+
+print()
