00f9d047caf766ef831acdf2b298c06a29a12df9
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-12 21:43:02 +0800

    style: Left-aligned pseudocode for code readability

--- simple_value_manipulations_to_filter_and_sort/is_unique_or_is_duplicate.py
+++ simple_value_manipulations_to_filter_and_sort/is_unique_or_is_duplicate.py
@@ -1,26 +1,25 @@
-# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. 
-# Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when 
+# Prog03: Create a program that ask user to input a number, continue asking 
+# until the user input is invalid. 
+# Display "Unique" after input when the inputted number don't have duplicate. 
+# Display "Duplicate" after input when 
 # the inputted number have duplicate.--is_unique_or_is_duplicate
 
-'''
-    set list_num
-    while loop
-        try-except
+# - set list_num
+# - while loop
+# - try-except
 
-            input number
+# - input number
 
-            if number in list_num
-                print duplicate
-            else
-                append number to list_num
-                print unique
+# - if number in list_num
+# - print duplicate
+# - else
+# - append number to list_num
+# - print unique
 
-        except ValueError
-            break loop
+# - except ValueError
+# - break loop
 
-
-'''
-
+# Main Program
 list_numbers = []
 
 while True:
@@ -29,9 +28,11 @@ while True:
 
         if number in list_numbers:
             print(f"{number} is a DUPLICATE!")
+
         else:
             list_numbers.append(number)
             print(f"{number} is UNIQUE!")
+
     except ValueError:
-        print("Program shuttind down...\n")
+        print("Program shutting down...Bye!\n")
         break
